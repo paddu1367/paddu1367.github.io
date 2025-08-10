68b143273026141b74002a157d8b44daf5895154
paddu1367 <paddupadmakar123@gmail.com>
2025-08-10 13:21:34 -0500

    edited enironment variables

--- pantry-inventory/script.js
+++ pantry-inventory/script.js
@@ -3,12 +3,12 @@
   import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
   const firebaseConfig = {
-    apiKey: "${API_KEY}",
-    authDomain: "${AUTH_DOMAIN}",
-    projectId: "${PROJECT_ID}",
-    storageBucket: "${STORAGE_BUCKET}",
-    messagingSenderId: "${MESSAGING_SENDER_ID}",
-    appId: "${APP_ID}"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
   };
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
